cf38ad1c7e89393c4e507c46122b38ee4093120e
Paul Roseau <paul.roseau@teads.tv>
2016-10-30 22:33:14 +0100

    Flesh out deleteUser

--- src/main/scala/exercise/controller/MainController.scala
+++ src/main/scala/exercise/controller/MainController.scala
@@ -59,15 +59,28 @@ class MainController[F[_]](implicit
       case None =>
         MF.pure(
           RouteResult.Complete(
-            userNotFound(uid).toHttp(StatusCodes.NotFound)
-          )
-        )
+            userNotFound(uid).toHttp(StatusCodes.NotFound)))
     }
   }
 
   def updateUser(uid: Long, entity: HttpEntity): F[RouteResult] = ???
 
-  def deleteUser(uid: Long): F[RouteResult] = ???
+  def deleteUser(uid: Long): F[RouteResult] = {
+    val res = 
+      Action.deleteUser(uid).foldMap(interpreter)
+
+    MF.flatMap(res) { 
+      case Some(_) =>
+        MF.pure(
+          RouteResult.Complete(
+            userUpdated(uid).toHttp(StatusCodes.OK)))
+      case None =>
+        MF.pure(
+          RouteResult.Complete(
+            userNotFound(uid).toHttp(StatusCodes.NotFound)))
+    }
+
+  }
 
   object Action {
 
@@ -75,6 +88,11 @@ class MainController[F[_]](implicit
       userOpt <- storeOps.getUser(uid)
       _ <- logOps.logUserRetrieval(uid, userOpt)
     } yield userOpt
+
+    def deleteUser(uid: Long): Free[Op, Option[Unit]] = for {
+      opt <- storeOps.deleteUser(uid)
+      _ <- logOps.logUserDeletion(uid, opt)
+    } yield opt
 
     def serializeUser(user: User): Free[Op, String] = for {
       jsonStr <- serializationOps.serializeUser(user)
@@ -86,6 +104,15 @@ object MainController {
 
   type Op0[T] = Coproduct[LogOp, SerializationOp, T]
   type Op[T] = Coproduct[StoreOp, Op0, T]
+
+  def userCreated(uid: Long) =
+    ResponseMessage("success", s"User id $uid created")
+
+  def userUpdated(uid: Long) =
+    ResponseMessage("success", s"User id $uid updated")
+
+  def userDeleted(uid: Long) =
+    ResponseMessage("success", s"User id $uid deleted")
 
   def userNotFound(uid: Long) =
     ResponseMessage("error", s"User id $uid does not reference any user")
