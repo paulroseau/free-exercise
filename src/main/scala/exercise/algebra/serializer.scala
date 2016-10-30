1f011ff45bbd3284dc5e80cf42c315a46d04d847
Paul Roseau <paul.roseau@teads.tv>
2016-10-30 22:51:06 +0100

    Changed error type from String -> DeserializationError in SerializationOp

--- src/main/scala/exercise/algebra/serializer.scala
+++ src/main/scala/exercise/algebra/serializer.scala
@@ -2,21 +2,25 @@ package exercise.algebra
 
 import scala.language.higherKinds
 
-import akka.http.scaladsl.model.{ HttpEntity, HttpResponse }
+import akka.http.scaladsl.model._
 
 import cats.free.{ Free, Inject }
 
 import exercise.model.User
 
 sealed trait SerializationOp[T]
-case class DeserializeUser(entity: HttpEntity) extends SerializationOp[Either[String, User]]
+case class DeserializeUser(entity: HttpEntity) extends SerializationOp[Either[DeserializationError, User]]
 case class SerializeUser(user: User) extends SerializationOp[String]
 
 class SerializationOps[F[_]](implicit ev: Inject[SerializationOp, F]) {
 
-  def deserializeUser(entity: HttpEntity): Free[F, Either[String, User]] =
+  def deserializeUser(entity: HttpEntity): Free[F, Either[DeserializationError, User]] =
     Free.inject(DeserializeUser(entity))
 
   def serializeUser(user: User): Free[F, String] =
     Free.inject(SerializeUser(user))
 }
+
+sealed trait DeserializationError
+case class UnsupportedContentType(contentType: ContentType) extends DeserializationError
+case class InvalidJson(details: String) extends DeserializationError
