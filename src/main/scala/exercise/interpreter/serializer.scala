9eee0bba0139db77bb154ebff7a9258bcb6ed30a
Paul Roseau <paul.roseau@teads.tv>
2016-10-30 23:31:31 +0100

    Fleshed serializer interpreter

--- src/main/scala/exercise/interpreter/serializer.scala
+++ src/main/scala/exercise/interpreter/serializer.scala
@@ -1,21 +1,56 @@
 package exercise.interpreter
 
-import scala.concurrent.{ ExecutionContext, Future }
+import java.nio.charset.Charset
 
-import cats.Id
+import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.duration._
+
+import argonaut._, Argonaut._, ArgonautShapeless._
+
+import akka.http.scaladsl.model._
+import akka.stream.Materializer
+import akka.stream.scaladsl.StreamConverters
+
 import cats.arrow.FunctionK
 
 import exercise.algebra._
+import exercise.model.User
 
 object SerializerInterpreter {
 
-  val futureInterpreter: FunctionK[SerializationOp, Future] =
+  def futureInterpreter(implicit
+    ec: ExecutionContext,
+    materializer: Materializer
+  ): FunctionK[SerializationOp, Future] =
     new FunctionK[SerializationOp, Future] {
+
+      val timeout = FiniteDuration(3, SECONDS)
 
       def apply[T](op: SerializationOp[T]): Future[T] =
         op match {
-          case DeserializeUser(entity) => ???
-          case SerializeUser(user) => ???
+
+          case SerializeUser(user) => Future(user.asJson.spaces2)
+
+          case DeserializeUser(entity) =>
+            entity.contentType match {
+              case ContentTypes.`application/json` =>
+                entity
+                  .toStrict(timeout)
+                  .map { x => 
+                    val contentEither =
+                      Right(x.data.decodeString(Charset.forName("UTF-8")))
+
+                    (for {
+                      rawContent <- contentEither.right
+                      json <- Parse.parse(rawContent).right
+                      res <- DecodeJson.of[User].decodeJson(json).result.right
+                    } yield res)
+                      .left
+                      .map(err => InvalidJson(err.toString))
+                  }
+            case ct => 
+              Future(Left(UnsupportedContentType(ct)))
+            }
         }
-    }
+  }
 }
